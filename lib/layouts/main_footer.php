2695a20eb178e7cf4e79087fd2333ffc3eef7ae2
JehanKandy <jehankandy@gmail.com>
2022-12-19 13:31:40 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -7,7 +7,14 @@
         <div class="title">Train Reservation System</div>
 
         <div class="grid">
-            <div class="t-item1">hi</div>
+            <div class="t-item1">
+                <div class="title">
+                    Train Reservation System
+                </div>
+                <div class="body">
+                    The Project Train Reservation System develop for educational purposes only
+                </div>
+            </div>
             <div class="t-item2">hi</div>
             <div class="t-item3">hi</div>
             <div class="t-item4">hi</div>
