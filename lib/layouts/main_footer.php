2f5ef5735bfef42cd8e601307746e0e334ab8e6d
JehanKandy <jehankandy@gmail.com>
2022-12-19 20:26:56 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -1,78 +1,58 @@
-<?php include("header.php"); ?>
-<link rel="stylesheet" href="../../css/style.css">
+<?php include("lib/layouts/header.php"); ?>
+<link rel="stylesheet" href="css/style.css">
 
-
-<div class="footer-content">
-    <div class="container">
-        <div class="title">Train Reservation System</div>
-
-        <div class="grid">
-            <div class="t-item1">
-                <div class="title">
-                    Train Reservation System
-                </div>
-                <div class="body">
-                    The Project Train Reservation System develop for educational purposes only. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa nulla beatae nam assumenda voluptatibus. Odio, eos vero earum nam id voluptatem repudiandae aliquam praesentium, ratione nihil culpa sequi iure quae!
-                </div>
-            </div>
-            <div class="t-item2">
-                <div class="title">
-                    Our Services 
-                </div>
-                <div class="body">
-                    <ul>
-                        <li><a href="#"> Train Time Table</a></li>
-                        <li><a href="#">Train Ticket Booking</a></li>
-                        <li><a href="#">Train Information</a></li>
-                        <li><a href="#">login and Registation</a></li>
-                    </ul>
-                </div></div>
-            <div class="t-item3">
-                <div class="title">
-                    Useful Links
-                </div>
-                <div class="body">
-                    <ul>
-                        <li><a href="#"> Train Time Table</a></li>
-                        <li><a href="#">Train Ticket Booking</a></li>
-                        <li><a href="#">Train Information</a></li>
-                        <li><a href="#">login and Registation</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="t-item4">
-                <div class="title">
-                    Address
-                </div>
-                <div class="body">
-                    <p><i class="fas fa-map-marked-alt"></i> &nbsp; 29/3/A, Megodagama, Menikhinna, Kandy, Sri Lanka.</p>
-                    <p><i class="fas fa-phone-alt"></i>&nbsp; +94 711758851</p>
-                    <p><i class="fas fa-envelope-open"></i> &nbsp; jehan@abc.com</p>
-                </div>
-            </div>
+<div class="main-content">
+    <div class="title">
+        Train Reservation System
+    </div>
+    <div class="quote">
+        Book You Trip with Us        
+    </div>
+    <div class="w3-show-inline-block">
+        <div class="w3-bar">
+            <a href=""><button class="book-trip-home-btn">Book A Ticket</button></a>
+            <a href=""><button class="login-home-btn">Login Here</button></a>
         </div>
-        <hr class="footer-hr">
-        <div class="footer-copy">
-            <div class="copy-body">
-                Copyright <i class="far fa-copyright"></i> Train Reservation System : 2022 All Right Reserved by <a href="https://github.com/JehanKandy" target="_blank" style="color: rgba(233, 178, 0, 0.89); text-decoration:none;">JehanKandy</a>
-            </div>
-            <div class="contact-icons">
-                <ul>
-                    <li>
-                        <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-github-square"></i></a>    
-                    </li>
-                    <li>
-                        <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-linkedin"></i></a>    
-                    </li>
-                    <li>
-                        <a href="#" class="btn-floating btn-sm text-white" style="font-size:23px;"><i class="fab fa-whatsapp-square"></i></a>    
-                    </li>
-                </ul>
-            </div>
+    </div>
+
+</div>
+<div class="menu-content">
+    <div class="container">
+        <div class="menu-grid">
+            <a href="#" style="text-decoration: none;"><div class="m-item1"><div class="text">Gallery</div></div></a>
+            <a href="#" style="text-decoration: none;"><div class="m-item2"><div class="text">About Us</div></div></a>
+            <a href="#" style="text-decoration: none;"><div class="m-item3"><div class="text">Train Route</div></div></a>
+            <a href="#" style="text-decoration: none;"><div class="m-item4"><div class="text">News</div></div></a>
         </div>
     </div>
 </div>
 
+<div class="train-bk">
+    <div class="container">
+        <div class="title">
+            Search a Train
+        </div>
+        <div class="body">
+            find nearest Train            
+        </div>
+        <a href=""><button class="find-train-btn">Find A Train</button></a>
+    </div>
+</div>
 
+<div class="follow-content">
+    <div class="container">
+        <div class="title">
+            Follow Us on
+        </div>
+        <div class="body">
+            <i class="fab fa-github" style="font-size: 50px;"></i>
+            <i class="fab fa-linkedin-in" style="font-size: 50px;"></i>
+            <i class="fab fa-facebook" style="font-size: 50px;"></i>
+            <i class="fab fa-instagram" style="font-size: 50px;"></i>
+            <i class="fab fa-youtube" style="font-size: 50px;"></i>
+        </div>
+    </div>
+</div>
 
-<script src="../../js/script.js"></script>
+<?php include("lib/layouts/main_footer.php"); ?>
+<script src="js/script.js"></script>
