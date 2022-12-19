6efa0b3a4109cb4f0b0c1ee365504e24bd0a6970
JehanKandy <jehankandy@gmail.com>
2022-12-19 12:53:10 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -1,7 +1,11 @@
 <?php include("header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 
-<div class="container">hi</div>
+<div class="container">
+    <div class="footer-content">
+        
+    </div>
+</div>
 
 
 <script src="../../js/script.js"></script>
