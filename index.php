6e796788ce3bd9451f567fa20bcb1450f9698291
JehanKandy <jehankandy@gmail.com>
2022-12-19 15:06:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <link rel="stylesheet" href="css/style.css">
 
 <div class="main-content">
-    
+    <div class="title">
+        Train Reservation System
+    </div>
 </div>
 
 <?php include("lib/layouts/main_footer.php"); ?>
