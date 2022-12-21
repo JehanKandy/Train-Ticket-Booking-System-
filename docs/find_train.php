bca58a04e37134c93cde39e207e41c1fefa0e96b
JehanKandy <jehankandy@gmail.com>
2022-12-21 20:41:08 +0530

    Update find_train.php

--- docs/find_train.php
+++ docs/find_train.php
@@ -1,6 +1,12 @@
 <?php include("../lib/layouts/header.php"); ?>
 <link rel="stylesheet" href="../css/style.css">
+<?php include("../lib/layouts/nav_bar.php"); ?>
 
+
+
+<div class="find-train-content">
+    hi
+</div>
 
 
 <script src="../js/script.js"></script>
