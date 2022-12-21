b57a4e3d113baf1d4e68aa9fd82ee65158839d02
JehanKandy <jehankandy@gmail.com>
2022-12-21 19:08:43 +0530

    Add files via upload

---
+++ lib/layouts/nav_bar.php
@@ -0,0 +1,26 @@
+<?php include("header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+<div class="nav-content">
+    <header class="header">
+        <div class="train-logo">Train Booking</div>
+        <div class="line-icon">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <div class="nav-bar">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Booking</a></li>
+                <li><a href="">Gallery</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Train Routes</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </div> 
+    </header>
+</div>
+
+<script src="../js/script.js"></script>
