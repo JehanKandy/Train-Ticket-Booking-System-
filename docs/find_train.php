3b3ed79c34407b6b398edb7424335df3b757c464
JehanKandy <jehankandy@gmail.com>
2022-12-22 15:51:13 +0530

    Update find_train.php

--- docs/find_train.php
+++ docs/find_train.php
@@ -43,10 +43,43 @@
 <div class="view-trains">
     <div class="container">
         <div class="title">
-            Avalibale Trains on 2022-12-27 From Kandy To Mathara
+            Avalibale Trains on 2022-12-27 From Kandy To Colombo
         </div>
         <div class="body">
-            hi
+            <table class="find-train-tbl">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Train Name</th>
+                        <th scope="col">Train Route</th>
+                        <th scope="col">Start Station</th>
+                        <th scope="col">Train Arrival</th>
+                        <th scope="col">Train Departure</th>
+                        <th scope="col">End Station</th>
+                        <th scope="col">Train Arrival</th>
+                        <th scope="col">Train Type</th>
+                        <th scope="col">Seat Availability</th>
+                        <th scope="col">Seat Type</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Podi Menike</td>
+                        <td>Badulla - Colombo</td>
+                        <td>Kandy</td>
+                        <td>3.45pm</td>
+                        <td>4.00pm</td>
+                        <td>Colombo</td>
+                        <td>7.00pm</td>
+                        <td>Express</td>
+                        <td>Avalibale</td>
+                        <td>1st Class, 2nd Class, 3rd Class</td>
+                        <td><a href="#"><button class="btn btn-primary">info</button></a></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
