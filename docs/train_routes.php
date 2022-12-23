db15bfc2f5425c640367a42035ebd11c067b58e2
JehanKandy <jehankandy@gmail.com>
2022-12-23 20:49:56 +0530

    Update train_routes.php

--- docs/train_routes.php
+++ docs/train_routes.php
@@ -2,6 +2,11 @@
 <link rel="stylesheet" href="../css/style.css">
 <?php include("../lib/layouts/nav_bar.php"); ?>
 
+<div class="train-routes">
+    <div class="container">
+        
+    </div>
+</div>
 
 <script src="../js/script.js"></script>
 <?php include("../lib/layouts/main_footer.php"); ?>
