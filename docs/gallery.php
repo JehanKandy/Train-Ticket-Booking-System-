dc36ba6c1dc7179a65b0156afe20ce1bb3ff05c5
JehanKandy <jehankandy@gmail.com>
2022-12-23 19:58:37 +0530

    Update gallery.php

--- docs/gallery.php
+++ docs/gallery.php
@@ -14,15 +14,15 @@
 <div class="train-gallery-content">
     <div class="container">
         <div class="gallery-grid">
-            <div class="g-item1">hi</div>
-            <div class="g-item2">hi</div>
-            <div class="g-item3">hi</div>
-            <div class="g-item4">hi</div>
-            <div class="g-item5">hi</div>
-            <div class="g-item6">hi</div>
-            <div class="g-item7">hi</div>
-            <div class="g-item8">hi</div>
-            <div class="g-item9">hi</div>
+            <div class="g-item1"></div>
+            <div class="g-item2"></div>
+            <div class="g-item3"></div>
+            <div class="g-item4"></div>
+            <div class="g-item5"></div>
+            <div class="g-item6"></div>
+            <div class="g-item7"></div>
+            <div class="g-item8"></div>
+            <div class="g-item9"></div>
 
         </div>
     </div>
