572df4b1306c3eed14ba19de339d4fe359f42427
JehanKandy <jehankandy@gmail.com>
2022-12-23 19:27:29 +0530

    Update gallery.php

--- docs/gallery.php
+++ docs/gallery.php
@@ -2,6 +2,12 @@
 <link rel="stylesheet" href="../css/style.css">
 <?php include("../lib/layouts/nav_bar.php"); ?>
 
+<div class="train-gallery">
+    <div class="container">
+        hi
+    </div>
+</div>
+
 
 <script src="../js/script.js"></script>
 <?php include("../lib/layouts/main_footer.php"); ?>
