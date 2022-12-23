122d880680a1c87dffb023d01366d462e5fb0090
JehanKandy <jehankandy@gmail.com>
2022-12-23 20:11:41 +0530

    Update about.php

--- docs/about.php
+++ docs/about.php
@@ -2,6 +2,12 @@
 <link rel="stylesheet" href="../css/style.css">
 <?php include("../lib/layouts/nav_bar.php"); ?>
 
+<div class="about-us">
+    <div class="container">
+        <div class="title">About Us</div>
+    </div>
+</div>
+
 
 <script src="../js/script.js"></script>
 <?php include("../lib/layouts/main_footer.php"); ?>
