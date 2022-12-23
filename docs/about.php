dfa4fcba6e2435f86c448edea083d681c0bf637b
JehanKandy <jehankandy@gmail.com>
2022-12-23 20:09:26 +0530

    Add files via upload

---
+++ docs/about.php
@@ -0,0 +1,7 @@
+<?php include("../lib/layouts/header.php");?>
+<link rel="stylesheet" href="../css/style.css">
+<?php include("../lib/layouts/nav_bar.php"); ?>
+
+
+<script src="../js/script.js"></script>
+<?php include("../lib/layouts/main_footer.php"); ?>
