7377bf6e72ec2c136b3e32bb5ecbf330353c2a54
JehanKandy <jehankandy@gmail.com>
2022-12-25 14:40:44 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -5,11 +5,26 @@
     <div class="body">
         <div class="title">Register Here</div>
         <hr>
-            <div class="reg-content">
-                jashgdk
-            </div>
-        <hr>
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
+                <div class="reg-content">
+                    <p class="input-text">Username : </p>
+                    <input type="text" name="username" id="logUsername" class="input-feild" placeholder="Username"><br>
 
+                    <p class="input-text">Email : </p>
+                    <input type="email" name="email" id="regemail" class="input-feild" placeholder="Email"><br>
+
+                    <p class="input-text">Password : </p>
+                    <input type="password" name="pass" id="regpass" class="input-feild" placeholder="Password"><br>
+
+                    <p class="input-text">Confirm Password : </p>
+                    <input type="password" name="cpass" id="regcpass" class="input-feild" placeholder="Confirm Password"><br>
+
+                    <span>
+                        <input type="reset" value="Clear" class="reset-btn">&nbsp;<input type="submit" value="Register" name="register" class="reg-btn">
+                    </span>
+                </div>
+            </form>
+        <hr>
     </div>
 </div>
 
