5e14822a5feb757c61c53e518049ab881ebf74aa
JehanKandy <jehankandy@gmail.com>
2022-12-25 19:44:09 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -41,6 +41,14 @@
                     </button>
             </div>"; 
         }
+        if($pass != $cpass){
+            return  "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+            </div>"; 
+        }
         
     }
 
