dadf6819697a2a6450aeff91f39e480bbd44bea3
JehanKandy <jehankandy@gmail.com>
2022-12-25 13:21:07 +0530

    Update login_footer.php

--- lib/layouts/login_footer.php
+++ lib/layouts/login_footer.php
@@ -1,8 +1,8 @@
 <?php include("header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 
-
+<hr class="login-footer-hr">
 <div class="login-footer-content">
-    JehanKandy
+Copyright <i class="far fa-copyright"></i> Train Reservation System : 2022 All Right Reserved by <a href="https://github.com/JehanKandy" target="_blank" style="text-decoration:none;">JehanKandy</a>
 </div>
 <script src="../../js/script.js"></script>
