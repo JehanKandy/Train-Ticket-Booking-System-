0dc993f3710b88881df458a9a52e1ba773c1ca58
JehanKandy <jehankandy@gmail.com>
2022-12-25 12:41:31 +0530

    Update news.php

--- docs/news.php
+++ docs/news.php
@@ -2,6 +2,13 @@
 <link rel="stylesheet" href="../css/style.css">
 <?php include("../lib/layouts/nav_bar.php"); ?>
 
+<div class="train-news">
+    <div class="container">
+        <div class="title">
+            NEWS
+        </div>
+    </div>
+</div>
 
 <script src="../js/script.js"></script>
 <?php include("../lib/layouts/main_footer.php"); ?>
