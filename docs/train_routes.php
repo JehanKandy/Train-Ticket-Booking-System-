c00df855d28aa9c227a50e48644e7ba223833e01
JehanKandy <jehankandy@gmail.com>
2022-12-25 12:24:53 +0530

    Update train_routes.php

--- docs/train_routes.php
+++ docs/train_routes.php
@@ -11,7 +11,36 @@
 </div>
 <div class="train-routes-content">
     <div class="container">
-        hi
+    <table class="find-train-tbl">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Train Name</th>
+                        <th scope="col">Train Route</th>
+                        <th scope="col">Start Station</th>
+                        <th scope="col">Start Time </th>
+                        <th scope="col">End Station</th>
+                        <th scope="col">Train Arrival</th>
+                        <th scope="col">Train Type</th>
+                        <th scope="col">Seat Type</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Podi Menike</td>
+                        <td>Badulla - Colombo</td>
+                        <td>Colombo</td>
+                        <td>6.30am</td>
+                        <td>Badulla</td>
+                        <td>5.00pc</td>
+                        <td>Express</td>
+                        <td>1st Class, 2nd Class, 3rd Class</td>
+                        <td><a href="#"><button class="btn btn-primary">info</button></a></td>
+                    </tr>
+                </tbody>
+            </table>
     </div>
 </div>
 
