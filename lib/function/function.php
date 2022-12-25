013f79c7a557ac77db51f0363cbc54511157954c
JehanKandy <jehankandy@gmail.com>
2022-12-25 19:40:35 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -8,6 +8,16 @@
 
     function reg_user($username, $email, $pass, $cpass){
         $con = Connection();
+
+        if(empty($username)){
+            return  "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+              <span aria-hidden='true'>&times;</span>
+            </button>
+          </div>";
+        }
+        
     }
 
 
