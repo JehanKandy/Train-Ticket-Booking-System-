77434644378eb490eebd3a3ff65c43c074bbf045
JehanKandy <jehankandy@gmail.com>
2022-12-25 14:08:45 +0530

    Add files via upload

---
+++ lib/views/reg.php
@@ -0,0 +1,6 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+
+<?php include("../layouts/login_footer.php"); ?>
+<script src="../../js/script.js"></script>
