cea482a451206c16ef6846d7625d0ca532fae96a
JehanKandy <jehankandy@gmail.com>
2022-12-25 13:22:29 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,6 +1,12 @@
 <?php include("../layouts/header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 
+<div class="train-login-content">
+    <div class="body">
+        Login Here
+    </div>
+</div>
+
 
 <?php include("../layouts/login_footer.php"); ?>
 <script src="../../js/script.js"></script>
