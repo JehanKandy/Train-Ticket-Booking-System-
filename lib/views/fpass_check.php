54dd709bf550d0ab3a4b39467c48fa7b7e784b02
JehanKandy <jehankandy@gmail.com>
2022-12-27 19:24:31 +0530

    Update fpass_check.php

--- lib/views/fpass_check.php
+++ lib/views/fpass_check.php
@@ -11,8 +11,9 @@
             <div class="body">
                 <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
                     <p class="input-text">Username : </p>
-
+                    <input type="text" name="username" id="fUser" class="input-feild" placeholder="Username"><br><br>
                     <p class="input-text">Email : </p>
+                    <input type="email" name="email" id="fEmail" class="input-feild" placeholder="Email"><br><br>
                 </form>
             </div>
         </div>
