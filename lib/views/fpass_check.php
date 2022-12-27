4fd7cbec09231cc7f9399cfb662d1c78e9694d81
JehanKandy <jehankandy@gmail.com>
2022-12-27 08:29:51 +0530

    Update fpass_check.php

--- lib/views/fpass_check.php
+++ lib/views/fpass_check.php
@@ -2,5 +2,16 @@
 <link rel="stylesheet" href="../../css/style.css">
 <?php include("../function/function.php"); ?>
 
+<div class="fpass-check">
+    <div class="continer">
+        <div class="fpass-check-content">
+            <div class="title">
+                Forget Password
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php include("../layouts/login_footer.php"); ?>
 <script src="../../js/script.js"></script>
