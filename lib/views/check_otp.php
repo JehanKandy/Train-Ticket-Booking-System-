8a80e79536135d609279a6f4f4b42433bb10bc9b
JehanKandy <jehankandy@gmail.com>
2022-12-30 18:24:13 +0530

    Add files via upload

---
+++ lib/views/check_otp.php
@@ -0,0 +1,7 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+
+
+<?php include("../layouts/login_footer.php"); ?>
+<script src="../../js/script.js"></script>
