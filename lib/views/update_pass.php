bb6c3dd74deab6b483fd6bb0087b0f65a024e805
JehanKandy <jehankandy@gmail.com>
2022-12-30 22:26:02 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -21,18 +21,15 @@
                     <input type="email" name="email" id="npassEmail" class="input-feild" placeholder="Email"><br>
 
                     <p class="input-text">New Password : </p>
-                    <input type="password" name="npass" id="npass" class="input-feild" placeholder="Password"><br>
+                    <input type="password" name="npass" id="npass" class="input-feild" placeholder="New Password"><br>
 
                     <p class="input-text">New Confirm Password : </p>
-                    <input type="password" name="ncpass" id="ncpass" class="input-feild" placeholder="Confirm Password"><br>
+                    <input type="password" name="ncpass" id="ncpass" class="input-feild" placeholder="Confirm New Password"><br>
 
-                    <span>
-                        <input type="reset" value="Clear" class="reset-btn">&nbsp;<input type="submit" value="Register" name="register" class="reg-btn">
-                    </span>
+                    <input type="submit" value="Reset Password" name="new_pass" class="new-pass-btn">
                 </div>
             </form>
         <hr>
-        Already Have an Account ? <a href="login.php" style="color: rgb(216, 55, 216); text-decoration: none;">Login</a>
         <div class="develop-by-reg"><i class="far fa-copyright"></i>&nbsp; JehanKandy</div>
     </div>
 </div>
