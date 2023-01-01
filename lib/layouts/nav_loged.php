1278f006bbe54aa9ee4f63c5bfb880f9eaeafda1
JehanKandy <jehankandy@gmail.com>
2023-01-01 06:34:37 +0530

    Update nav_loged.php

--- lib/layouts/nav_loged.php
+++ lib/layouts/nav_loged.php
@@ -7,12 +7,12 @@
 ?>
 
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #ebebeb;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" href="#">&nbsp;&nbsp;Train Booking System</a>
+    <a class="navbar-brand" style="color: black;" href="#">&nbsp;&nbsp;Train Booking System</a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
     </ul>
     <span class="navbar-text">
