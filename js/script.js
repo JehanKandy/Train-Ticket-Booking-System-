4ca4ebb6f9334874b337886b00e97dc2dfc7d75d
JehanKandy <jehankandy@gmail.com>
2023-01-01 06:28:50 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const menu_toggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menu_toggle.addEventListener('click', () => {
+    menu_toggle.classList.toggle('is-active');
+    sidebar.classList.toggle('is-active');
+});
+
+$(document).ready(function() {
+    $('#myTable').DataTable();
+});
